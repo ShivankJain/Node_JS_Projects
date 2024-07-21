9b9ddd9002ecf7a30e751388aaddaed07d9f2c81
shivank55 <shivankjain555@gmail.com>
2024-07-21 12:59:17 +0530

    Progress...

--- index.js
+++ index.js
@@ -6,12 +6,28 @@ const PORT = 8000
 
 // Routes
 
-app.get("/",(req,res)=>{
-    return res.json('HomePage')
+// Example of an hybrid server 
+
+app.get('/users',(req,res)=>{
+  const html = `
+     <ul>
+     ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+     </ul>
+  `
+  res.send(html)    
 })
 
-app.get("/users",(req,res)=>{
+// REST API
+app.get("/api/users",(req,res)=>{
   return res.json(users)
+})
+
+//Express feature for variable|dynamic parameter  " : "
+
+app.get('/api/users/:id',(req,res)=>{ 
+    const id = Number(req.params.id)
+    const user = users.find((user) => user.id === id)    
+    return res.json(user)
 })
 
 
