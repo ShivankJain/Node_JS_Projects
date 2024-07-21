a127fb99250c948a4b1158de1f6d3608b288c20a
shivank55 <shivankjain555@gmail.com>
2024-07-21 12:12:47 +0530

    Progress...

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express")
+const users = require("./MOCK_DATA.json")
 
 const app = express()
 const PORT = 8000
+
+// Routes
+
+app.get("/",(req,res)=>{
+    return res.json('HomePage')
+})
+
+app.get("/users",(req,res)=>{
+  return res.json(users)
+})
+
 
 
 app.listen(PORT,()=>console.log(`Server Started At PORT: ${PORT}`))
